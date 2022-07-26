990ac65cff1545205cf2a63f6377f5bee707bef4
Joseph Loper <106849853+jd-loper@users.noreply.github.com>
2022-07-25 23:16:00 -0700

    Print output with innerHTML

--- javascript.js
+++ javascript.js
@@ -1,9 +1,8 @@
 let input = prompt("Enter a word:", "Bacon");
 let firstLetter = input.charAt(0);
 let word = input.substring(1, input.length);
+let newWord = word+=firstLetter+="ay";
 
-function output() {
-    console.log(word+=firstLetter+="ay");
+window.onload = function() {
+    document.getElementById("newValue").innerHTML = newWord.toUpperCase();
 }
-
-output(input);
